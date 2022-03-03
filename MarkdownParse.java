80bcb6d88f701fe190ab36181c5a8566eaa5b04d
ndesopo <97704645+ndesopo@users.noreply.github.com>
2022-03-03 14:11:43 -0800

    updated main method
    bruh

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -47,9 +47,9 @@ public class MarkdownParse {
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
+        Path fileName = Path.of(args[0]);
+        // String contents = Files.readString(fileName);
+        Map<String, List<String>> links = getLinks(fileName.toFile());
         System.out.println(links);
     }
 }
